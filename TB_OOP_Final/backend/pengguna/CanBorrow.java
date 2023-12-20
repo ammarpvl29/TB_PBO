fa5f81876808aafbbaed38dfc6a9dd402b68c8d7
ammarpvl29 <123556107+ammarpvl29@users.noreply.github.com>
2023-12-20 07:35:49 +0700

    Delete TB_OOP_Final directory

--- TB_OOP_Final/backend/pengguna/CanBorrow.java
+++
@@ -1,8 +0,0 @@
-package assignments.assignment4.backend.pengguna;
-
-import assignments.assignment4.backend.buku.Buku;
-
-public interface CanBorrow {
-    public String pinjam(Buku buku, String tanggalPeminjaman);
-    public String kembali(Buku buku, String tanggalPengembalian);
-}
